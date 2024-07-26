29274689ebe957264cc3023165a99ae12a0ee96c
SankaraNarayanan-Baskaran <sankaranarayanan.baskaran@bounteous.com>
2024-07-26 16:37:30 +0530

    finished commit

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Author,Post,Tag,Comment
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug":("title",)}
+    list_filter = ("tags","author","date",)
+    list_display = ("title","author","date",)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display=("user_name","post")
+
+admin.site.register(Author)
+admin.site.register(Post,PostAdmin)
+admin.site.register(Tag)
+admin.site.register(Comment,CommentAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,92 +1,111 @@
-from django.shortcuts import render
+from typing import Any
+from django.shortcuts import render, get_object_or_404
 from datetime import date
-all_posts = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Sank",
-        "date": date(2024, 4, 21),
-        "title": "Mountain Hiking",
-        "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Sank",
-        "date": date(2024, 4, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Sank",
-        "date": date(2024, 4, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
-]
-
-def get_date(post):
-    return post['date']
-# Create your views here.
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from .models import Author, Tag, Post
+from django.views.generic import ListView, DetailView
+from django.views import View
+from .forms import CommentForm
+# def starting_page(request):
+#     latest_posts = Post.objects.all().order_by("-date")[:3]
+#     return render(request, "blog/index.html",{
+#         "posts":latest_posts
+#     })
 
 
-def starting_page(request):
-    sorted_posts=sorted(all_posts,key=get_date)
-    latest_posts=sorted_posts[-3:]
-    return render(request, "blog/index.html",{
-        "posts":latest_posts
-    })
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    context_object_name = "posts"
+    queryset = Post.objects.all().order_by("-date")[:3]
+
+# def posts(request):
+#     all_posts = Post.objects.all().order_by("-date")
+#     return render(request, "blog/all-posts.html",{
+#         "all_posts":all_posts
+#     })
 
 
-def posts(request):
-    return render(request, "blog/all-posts.html",{
-        "all_posts":all_posts
-    })
+class PostsView(ListView):
+    template_name = "blog/all-posts.html"
+    context_object_name = "all_posts"
+    queryset = Post.objects.all().order_by("-date")
+
+# def post_detail(request, slug):
+#     identified_post=get_object_or_404(Post,slug=slug)
+#     return render(request, "blog/post-detail.html",{
+#         "post":identified_post,
+#         "post_tags":identified_post.tags.all()
+#     })
 
 
-def post_detail(request, slug):
-    identified_post=next(post for post in all_posts if post['slug'] == slug)
-    return render(request, "blog/post-detail.html",{
-        "post":identified_post
-    })
+class PostDetailView(View):
+    # model=Post
+    # template_name = "blog/post-detail.html"
+    def is_stored_post(self,request,post_id):
+         stored_posts=request.session.get("stored_posts")
+         if stored_posts is not None:
+                is_saved_for_later = post_id in stored_posts
+         else:
+             is_saved_for_later=False
+         return is_saved_for_later
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+     
+        context = {"post": post,
+                   "post_tags": post.tags.all(),
+                   "comment_form": CommentForm(),
+                   "comments": post.comments.all().order_by("-id"),
+                   "saved_for_later":self.is_stored_post(request,post.id)}
+        
+        return render(request, "blog/post-detail.html", context)
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+        context = {"post": post,
+                   "post_tags": post.tags.all(),
+                   "comment_form": comment_form,
+                     "comments": post.comments.all().order_by("-id"),
+                      "saved_for_later":self.is_stored_post(request,post.id)}
+        return render(request, "blog/post-detail.html", context)
+    # context_object_name = "post"
+    # slug_field = "slug"
+    # slug_url_kwarg = "slug"
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context["post_tags"] = self.object.tags.all()
+    #     context["comment_form"] = CommentForm()
+    #     return context
+
+
+class ReadLater(View):
+
+    def get(self,request):
+                stored_posts = request.session.get("stored_posts")
+                context = {}
+                if stored_posts is None or len(stored_posts) == 0:
+                     context["posts"] =[]
+                     context["has_posts"] = False
+                else:
+                    posts= Post.objects.filter(id__in=stored_posts)
+                    context["posts"]=posts
+                    context["has_posts"]=True
+                return render(request,"blog/stored-posts.html",context)
+    def post(self,request):
+        stored_posts = request.session.get("stored_posts")
+        if stored_posts is None:
+            stored_posts = []
+        post_id = int(request.POST["post_id"])
+        if not post_id in stored_posts:   
+            stored_posts.append(post_id)
+
+        else:
+             stored_posts.remove(post_id)
+        request.session["stored_posts"] = stored_posts
+        return HttpResponseRedirect("/")
